b25aaca684afaafb01ecf2c003530363d22d9b56
Adam Gray <agray@qa.com>
2023-11-23 12:23:16 +0000

    temp hardcode ip

--- test-app.py
+++ test-app.py
@@ -3,14 +3,14 @@ import requests
 from os import getenv
 
 class Task1TestCase(unittest.TestCase):
-    STAGING_IP = '34.89.76.112'
+    STAGING_URL = 'http://34.89.76.112'
 
     def test_200_response(self):
-        response = requests.get(f'http://{self.STAGING_IP}')
+        response = requests.get(url=self.STAGING_URL)
         self.assertEqual(response.status_code, 200)
     
     def test_your_name_displayed(self):
-        response = requests.get(f'http://{self.STAGING_IP}')
+        response = requests.get(url=self.STAGING_URL)
         self.assertIn(b'{0}'.format(getenv('YOUR_NAME')), response.text)
     
 if __name__ == '__main__':
